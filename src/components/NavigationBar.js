27255810ca9e531152980cc4b1af625ff7408d1d
Yasela-dissanayake <yasela2014@gmail.com>
2024-01-18 09:43:57 +0530

    feat:BS navbar added

---
+++ src/components/NavigationBar.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+const NavigationBar = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Navbar
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link " aria-current="page" href="#">
+                  Job Vacancy
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Features
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Pricing
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Abou Us
+                </a>
+              </li>
+            </ul>
+            <form className="d-flex">
+              <button className="btn btn-outline-success" type="submit">
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default NavigationBar;
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,7 +1,16 @@
 import React from "react";
+import NavigationBar from "../components/NavigationBar";
 
 const Homepage = () => {
-  return <div>Homepage</div>;
+  return (
+    <div>
+      <header>
+        <NavigationBar />
+      </header>
+      <main></main>
+      <footer></footer>
+    </div>
+  );
 };
 
 export default Homepage;
