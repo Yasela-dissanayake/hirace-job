8f56fa313710a824a79153d851b61fef49843d45
Yasela-dissanayake <yasela2014@gmail.com>
2024-01-18 09:46:56 +0530

    feat:all the images added

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -6,7 +6,7 @@ const NavigationBar = () => {
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
           <a className="navbar-brand" href="#">
-            Navbar
+            <img src={brand} />
           </a>
           <button
             className="navbar-toggler"
