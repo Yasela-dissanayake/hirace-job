83bb0d3d5d61d09815de96fd64db98a2182c319d
Yasela-dissanayake <yasela2014@gmail.com>
2024-01-20 14:32:08 +0530

    feat:BS  tabs added

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,6 +1,8 @@
 import React from "react";
 import NavigationBar from "../components/NavigationBar";
 import classes from "./Homepage.module.css";
+import "./Homepage.module.css";
+
 import heroImg from "../images/img1.png";
 import PrimaryButton from "../components/PrimaryButton";
 import portfolio1 from "../images/portfolio1.png";
@@ -393,7 +395,72 @@ const Homepage = () => {
             </div>
           </div>
         </section>
-        <section id="job-vacancy"></section>
+        <section id="job-vacancy">
+          <div className={`container  ${classes.vacancyContainer}`}>
+            <ul
+              className={`nav nav-pills mb-3 ${classes.navPills}`}
+              id="pills-tab"
+              role="tablist"
+            >
+              <li
+                className={`nav-item ${classes.tabButton}`}
+                role="presentation"
+              >
+                <button
+                  className={`nav-link active `}
+                  id="pills-home-tab"
+                  data-bs-toggle="pill"
+                  data-bs-target="#pills-home"
+                  type="button"
+                  role="tab"
+                  aria-controls="pills-home"
+                  aria-selected="true"
+                >
+                  Popular
+                </button>
+              </li>
+              <li className="nav-item" role="presentation">
+                <button
+                  className="nav-link"
+                  id="pills-profile-tab"
+                  data-bs-toggle="pill"
+                  data-bs-target="#pills-profile"
+                  type="button"
+                  role="tab"
+                  aria-controls="pills-profile"
+                  aria-selected="false"
+                >
+                  Latest
+                </button>
+              </li>
+            </ul>
+            <div className="tab-content" id="pills-tabContent">
+              <div
+                className="tab-pane fade show active"
+                id="pills-home"
+                role="tabpanel"
+                aria-labelledby="pills-home-tab"
+              >
+                <div className={`row row-cols-2`}>
+                  <div className={`col`}>a</div>
+                  <div className={`col`}>a</div>
+                  <div className={`col`}>a</div>
+                  <div className={`col`}>a</div>
+                  <div className={`col`}>a</div>
+                  <div className={`col`}>a</div>
+                </div>
+              </div>
+              <div
+                className="tab-pane fade"
+                id="pills-profile"
+                role="tabpanel"
+                aria-labelledby="pills-profile-tab"
+              >
+                ...cxcxvxcvxvxcv
+              </div>
+            </div>
+          </div>
+        </section>
       </main>
       <footer></footer>
     </div>
