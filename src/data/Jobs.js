e15c26d990723accb2111492c46de79058e561c3
Yasela-dissanayake <yasela2014@gmail.com>
2024-01-20 17:21:20 +0530

    feat:dynamic tab content adding done

---
+++ src/data/Jobs.js
@@ -0,0 +1,69 @@
+import loom from "../images/loom.png";
+import hubspot from "../images/hubspot.png";
+import framer from "../images/framer.png";
+import notion from "../images/notion.png";
+import spotify from "../images/spotify.png";
+import twitter from "../images/twitter.png";
+
+export const Jobs = [
+  {
+    title: "Web Developer",
+    subtitle: "Loom",
+    icon: loom,
+    para: "We are on a mission to empower everyone at work to communicate more effectively.",
+    location: "New York",
+    salary: "75k - 90k / Year",
+    jobtype: "Full-Time",
+    date: "5 Jan 2022",
+  },
+  {
+    title: "Senior DevOps Engineer",
+    subtitle: "Hubspot",
+    icon: hubspot,
+    para: "Hubspot is a leading customer relationship management platform for scaling companies",
+    location: "California",
+    salary: "75k - 90k / Year",
+    jobtype: "Contract",
+    date: "7 Jan 2022",
+  },
+  {
+    title: "Product Designer",
+    subtitle: "Framer",
+    icon: framer,
+    para: "We are Framer. We're building the web dsign platform to help you do that.",
+    location: "San Diego",
+    salary: "75k - 90k / Year",
+    jobtype: "Part-Time",
+    date: "23 Jan 2022",
+  },
+  {
+    title: "Frontend Engineer",
+    subtitle: "Notion",
+    icon: notion,
+    para: "You all-in-one workspace for tasksm notes, wikis, databases, and team collaboration",
+    location: "New York",
+    salary: "10k - 18k / Month",
+    jobtype: "Internship",
+    date: "15 Jan 2022",
+  },
+  {
+    title: "Data Scientist",
+    subtitle: "Spotify",
+    icon: spotify,
+    para: "Passionate music fans. Innovative tech pros. Perfect harmony. Join our band.",
+    location: "California",
+    salary: "75k - 90k / Year",
+    jobtype: "Full-Time",
+    date: "2 Feb 2022",
+  },
+  {
+    title: "UX Researcher",
+    subtitle: "Twitter",
+    icon: twitter,
+    para: "Twitter is the market-leading linking platform that was the first of its kind.",
+    location: "California",
+    salary: "8k - 15k / Month",
+    jobtype: "Internship",
+    date: "13 Feb 2022",
+  },
+];
